4a197582a1d7ccb8c3dc4b092b2bdde19534128e
Alvaro Deveze <alvaro.deveze@live.com.ar>
2022-05-03 11:41:56 -0300

    parte 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AcercaComponent } from './acerca/acerca.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AcercaComponent,
+    SkillsComponent,
+    ProyectosComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule
